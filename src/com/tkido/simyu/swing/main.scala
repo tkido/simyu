61e9ad79c8a85451a3461cd0112295361a28e376
tkido <takanorikido@gmail.com>
2015-03-26 09:48:07 +0900

    resource

--- src/com/tkido/simyu/swing/main.scala
+++ src/com/tkido/simyu/swing/main.scala
@@ -1,6 +1,7 @@
 package com.tkido.simyu.swing
 
 import java.awt.Cursor
+import javax.imageio.ImageIO
 
 import scala.swing.Dimension
 import scala.swing.MainFrame
@@ -8,15 +9,13 @@ import scala.swing.Menu
 import scala.swing.MenuBar
 import scala.swing.SimpleSwingApplication
 
-import javax.imageio.ImageIO
-
 object main extends SimpleSwingApplication {
   def top = new MainFrame {
     title = "シムゆっくり"
     minimumSize = new Dimension( 800, 480 )
     cursor = new Cursor( Cursor.HAND_CURSOR )
     resizable = false
-    //iconImage = ImageIO.read( getClass().getResourceAsStream( "icon.bmp" ) )
+    iconImage = ImageIO.read( getClass().getResourceAsStream( "/resource/favicon.bmp" ) )
     menuBar = new MenuBar() {
       contents += new Menu( "ゆっくり" )
       contents += new Menu( "アイテム" )
