515cf86702ea3a4b7fa0651ca59e9ab6886fb0f9
tkido <takanorikido@gmail.com>
2015-04-17 22:56:18 +0900

    status s

--- src/com/tkido/game/command.scala
+++ src/com/tkido/game/command.scala
@@ -10,3 +10,17 @@ case object Pass extends AppCommand
 case object Create extends AppCommand
 case object Status extends AppCommand
 
+
+
+object CommandParser {
+  def apply(arg:String) :Option[Command] = {
+    arg.trim.stripLineEnd.toLowerCase match {
+      case ""       => Some(Pass)
+      case "quit"   => Some(Quit)
+      case "create" => Some(Create)
+      case "status" => Some(Status)
+      case "s"      => Some(Status)
+      case _        => None
+    }
+  }
+}
