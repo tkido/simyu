064b082321cea620617bc3959d48536e36730e49
tkido <takanorikido@gmail.com>
2015-04-18 20:46:48 +0900

    case class

--- src/com/tkido/simyu/action.scala
+++ src/com/tkido/simyu/action.scala
@@ -3,9 +3,12 @@ package com.tkido.simyu
 import com.tkido.simyu.simulation.Actor
 import com.tkido.simyu.simulation.Thing
 
-abstract sealed class Action(actor:Actor)
-abstract sealed class TargetAction(actor:Actor, target:Thing) extends Action(actor)
+abstract sealed class Action{
+  val actor:Actor
+}
+abstract sealed class TargetAction extends Action{
+  val target:Thing
+}
 
-case class PassAction(actor:Actor) extends Action(actor)
-case class GreetingAction(actor:Actor, target:Thing) extends TargetAction(actor, target)
-
+case class PassAction(actor:Actor) extends Action
+case class GreetingAction(actor:Actor, target:Thing) extends TargetAction
--- src/com/tkido/simyu/language/translator.scala
+++ src/com/tkido/simyu/language/translator.scala
@@ -1,15 +1,15 @@
 package com.tkido.simyu.language
 
 import com.tkido.simyu._
-import com.tkido.simyu.simulation.Actor
+import com.tkido.simyu.simulation._
 
 object Translator {
   def apply(action:Action) :String = {
-    val (actor, content) = action match{
-      case PassAction(a) => (a, a.label + "はゆっくりしているよ！")
-      case GreetingAction(a, t) => (a, t.label + "ゆっくりしていってね！！！")
+    val content = action match{
+      case PassAction(a) => a.label + "はゆっくりしているよ！"
+      case GreetingAction(a, t) => t.label + "ゆっくりしていってね！！！"
       //case _ => ()
     }
-    """%s「%s」""".format(actor.label, content)
+    """%s「%s」""".format(action.actor.label, content)
   }
 }
